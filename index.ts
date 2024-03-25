72a6f6b6ee2eb0001e4e56a8ea4ff00f617a7d68
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-25 13:46:16 +0500

    promptSync
    promptSync

---
+++ index.ts
@@ -0,0 +1,17 @@
+import promptSync from 'prompt-sync'
+const prompt = promptSync();
+
+// Name :
+let userInput = prompt('Enter your Name: ');
+console.log('your name is '+ userInput);
+
+
+//Subtraction :
+//let num1 = parseInt(prompt('enter 1st number'));
+//let num2 = parseInt(prompt('enter 2st number'));
+
+//console.log( num1 +num2);
+
+/*
+In TypeScript, parseInt is a function that converts
+ a string to an integer with an optional radix.*/
